c02a2fd51d0cd169c95f2fe45a1cc334083835ed
Michel907 <michelbrimo@gmail.com>
2023-10-01 16:31:18 +0300

    testing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Routing\Annotation\Route as AnnotationRoute;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/hello', function () {
     return view('welcome');
 });
+
+//this is a test run
